7ff853622a7ace5b4525c7f6d3eca1b0445c815f
Chau Tran <nartc7789@gmail.com>
2023-01-31 12:13:11 -0600

    feat: add gltf loader

---
+++ libs/angular-three-soba/loaders/src/lib/gltf-loader/gltf-loader.ts
@@ -0,0 +1,65 @@
+import { injectNgtLoader, NgtLoaderResults, NgtObjectMap } from 'angular-three';
+import { Observable, take } from 'rxjs';
+// @ts-ignore
+import { MeshoptDecoder } from 'three-stdlib';
+import { DRACOLoader } from 'three-stdlib/loaders/DRACOLoader';
+import { GLTF, GLTFLoader } from 'three-stdlib/loaders/GLTFLoader';
+
+let dracoLoader: DRACOLoader | null = null;
+
+function _extensions(useDraco: boolean | string, useMeshOpt: boolean, extensions?: (loader: GLTFLoader) => void) {
+    return (loader: THREE.Loader) => {
+        if (extensions) {
+            extensions(loader as GLTFLoader);
+        }
+
+        if (useDraco) {
+            if (!dracoLoader) {
+                dracoLoader = new DRACOLoader();
+            }
+
+            dracoLoader.setDecoderPath(
+                typeof useDraco === 'string' ? useDraco : 'https://www.gstatic.com/draco/versioned/decoders/1.4.3/'
+            );
+            (loader as GLTFLoader).setDRACOLoader(dracoLoader);
+        }
+
+        if (useMeshOpt) {
+            (loader as GLTFLoader).setMeshoptDecoder(
+                typeof MeshoptDecoder === 'function' ? MeshoptDecoder() : MeshoptDecoder
+            );
+        }
+    };
+}
+
+export interface NgtsGLTFLoader {
+    <TInput extends string | string[] | Record<string, string>>(
+        path: TInput | Observable<TInput>,
+        useDraco?: boolean | string,
+        useMeshOpt?: boolean,
+        extensions?: (loader: GLTFLoader) => void
+    ): Observable<NgtLoaderResults<TInput, GLTF & NgtObjectMap>>;
+    preload: <TInput extends string | string[] | Record<string, string>>(
+        path: TInput | Observable<TInput>,
+        useDraco?: boolean | string,
+        useMeshOpt?: boolean,
+        extensions?: (loader: GLTFLoader) => void
+    ) => void;
+}
+
+function injectGLTFLoader<TInput extends string | string[] | Record<string, string>>(
+    path: TInput | Observable<TInput>,
+    useDraco: boolean | string = true,
+    useMeshOpt = true,
+    extensions?: (loader: GLTFLoader) => void
+): Observable<NgtLoaderResults<TInput, GLTF & NgtObjectMap>> {
+    return injectNgtLoader(() => GLTFLoader, path, _extensions(useDraco, useMeshOpt, extensions)) as Observable<
+        NgtLoaderResults<TInput, GLTF & NgtObjectMap>
+    >;
+}
+
+(injectGLTFLoader as NgtsGLTFLoader).preload = (path, useDraco = true, useMeshOpt = true, extensions) => {
+    injectGLTFLoader(path, useDraco, useMeshOpt, extensions).pipe(take(1)).subscribe();
+};
+
+export const injectNgtsGLTFLoader = injectGLTFLoader as NgtsGLTFLoader;
