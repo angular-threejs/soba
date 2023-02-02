7b58861d24aa3bcb13505c4f1e4410cf6737d4dc
Chau Tran <nartc7789@gmail.com>
2023-02-02 13:20:25 -0600

    feat: add detailed and stats

---
+++ libs/angular-three-soba/performance/src/lib/detailed/detailed.ts
@@ -0,0 +1,37 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit } from '@angular/core';
+import { extend, injectNgtRef, NgtRxStore } from 'angular-three';
+import { combineLatest } from 'rxjs';
+import { LOD } from 'three';
+
+extend({ LOD });
+
+@Component({
+    selector: 'ngts-detailed[distances]',
+    standalone: true,
+    template: `
+        <ngt-lOD [ref]="lodRef" ngtCompound (beforeRender)="$any($event).object.update($any($event).state.camera)">
+            <ng-content />
+        </ngt-lOD>
+    `,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class NgtsDetailed extends NgtRxStore implements OnInit {
+    @Input() lodRef = injectNgtRef<LOD>();
+
+    @Input() set distances(distances: number[]) {
+        this.set({ distances });
+    }
+
+    ngOnInit() {
+        this.updateLodChildren();
+    }
+
+    private updateLodChildren() {
+        this.hold(combineLatest([this.lodRef.children$(), this.select('distances')]), ([children, distances]) => {
+            this.lodRef.nativeElement.levels.length = 0;
+            children.forEach((child, index) => {
+                this.lodRef.nativeElement.addLevel(child, distances[index]);
+            });
+        });
+    }
+}
