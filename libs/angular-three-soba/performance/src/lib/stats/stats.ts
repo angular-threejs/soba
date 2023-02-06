a348fde0b9219986a80f487e66a5527989ba2fe9
Chau Tran <nartc7789@gmail.com>
2023-02-06 12:47:25 -0600

    fix: allow stats to position right or left

--- libs/angular-three-soba/performance/src/lib/stats/stats.ts
+++ libs/angular-three-soba/performance/src/lib/stats/stats.ts
@@ -1,6 +1,6 @@
 import { DOCUMENT } from '@angular/common';
-import { Directive, inject, Input, OnInit } from '@angular/core';
-import { addAfterEffect, addEffect, NgtAnyRecord, NgtInjectedRef, NgtRxStore, startWithUndefined } from 'angular-three';
+import { Directive, ElementRef, inject, Input, OnInit } from '@angular/core';
+import { addAfterEffect, addEffect, is, NgtAnyRecord, NgtRxStore, startWithUndefined } from 'angular-three';
 import { combineLatest } from 'rxjs';
 import * as Stats from 'stats.js';
 
@@ -10,7 +10,7 @@ import * as Stats from 'stats.js';
 })
 export class NgtsStats extends NgtRxStore implements OnInit {
     private readonly document = inject(DOCUMENT);
-    private readonly stats = (Stats as NgtAnyRecord)['default']
+    private readonly stats: Stats = (Stats as NgtAnyRecord)['default']
         ? new (Stats as NgtAnyRecord)['default']()
         : new Stats();
 
@@ -18,32 +18,44 @@ export class NgtsStats extends NgtRxStore implements OnInit {
         this.set({ showPanel: showPanel === undefined ? this.get('showPanel') : showPanel });
     }
 
-    @Input() set parent(parent: NgtInjectedRef<HTMLElement>) {
-        this.set({ parent });
+    @Input() set parent(parent: HTMLElement | ElementRef<HTMLElement>) {
+        this.set({ parent: is.ref(parent) ? parent.nativeElement : parent });
     }
 
     @Input() set classes(classes: string) {
         this.set({ classes });
     }
 
+    @Input() set right(right: boolean) {
+        this.set({ right });
+    }
+
     override initialize(): void {
         super.initialize();
-        this.set({ showPanel: 0 });
+        this.set({ showPanel: 0, right: false });
     }
 
     ngOnInit() {
         this.effect(
             combineLatest([
                 this.select('showPanel'),
+                this.select('right'),
                 this.select('parent').pipe(startWithUndefined()),
                 this.select('classes').pipe(startWithUndefined()),
             ]),
-            ([showPanel, parent, classes]) => {
-                const node = parent && parent.nativeElement ? parent.nativeElement : this.document.body;
+            ([showPanel, right, parent, classes]) => {
+                const node = parent ?? this.document.body;
                 this.stats.showPanel(showPanel);
                 node.appendChild(this.stats.dom);
                 if (classes) {
                     this.stats.dom.classList.add(...classes.split(' ').filter((cls: string) => cls));
+                }
+                if (right) {
+                    this.stats.dom.style.right = '0px';
+                    this.stats.dom.style.left = 'inherit';
+                } else {
+                    this.stats.dom.style.left = '0px';
+                    this.stats.dom.style.right = 'inherit';
                 }
                 const begin = addEffect(() => this.stats.begin());
                 const end = addAfterEffect(() => this.stats.end());
