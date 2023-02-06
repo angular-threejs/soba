7705ba828fea1b124a8ce357a6933c99a5909a7e
Chau Tran <nartc7789@gmail.com>
2023-02-06 15:21:52 -0600

    docs: add text stories

---
+++ libs/angular-three-soba/src/abstractions/text.stories.ts
@@ -0,0 +1,285 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { extend } from 'angular-three';
+import { NgtsText } from 'angular-three-soba/abstractions';
+import { DoubleSide, MeshBasicMaterial } from 'three';
+import { makeCanvasOptions, StorybookSetup, turn } from '../setup-canvas';
+
+extend({ MeshBasicMaterial });
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'left'"
+            [font]="'https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            (beforeRender)="turn($any($event).object)"
+        >
+            <ngt-mesh-basic-material [color]="color" [side]="DoubleSide" [transparent]="true" [opacity]="1" />
+        </ngts-text>
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class CustomMaterialTextStory {
+    @Input() text = '';
+    @Input() color = 'turquoise';
+    readonly turn = turn;
+    readonly DoubleSide = DoubleSide;
+}
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [color]="'#EC2D2D'"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'right'"
+            [direction]="'auto'"
+            [font]="'https://fonts.gstatic.com/s/scheherazade/v20/YA9Ur0yF4ETZN60keViq1kQgtA.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            (beforeRender)="turn($any($event).object)"
+        />
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class LTRTextStory {
+    @Input() text = '';
+    readonly turn = turn;
+}
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [color]="'#EC2D2D'"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'left'"
+            [font]="'https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            [outlineOffsetX]="'10%'"
+            [outlineOffsetY]="'10%'"
+            [outlineBlur]="'30%'"
+            [outlineOpacity]="0.3"
+            [outlineColor]="'#EC2D2D'"
+            (beforeRender)="turn($any($event).object)"
+        />
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class ShadowTextStory {
+    @Input() text = '';
+    readonly turn = turn;
+}
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [color]="'#EC2D2D'"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'left'"
+            [font]="'https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            [strokeWidth]="'2.5%'"
+            [strokeColor]="'#fff'"
+            (beforeRender)="turn($any($event).object)"
+        />
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class StrokeTextStory {
+    @Input() text = '';
+    readonly turn = turn;
+}
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [color]="'#EC2D2D'"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'left'"
+            [font]="'https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            [outlineWidth]="2"
+            [outlineColor]="'#fff'"
+            (beforeRender)="turn($any($event).object)"
+        />
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class OutlineTextStory {
+    @Input() text = '';
+    readonly turn = turn;
+}
+
+@Component({
+    standalone: true,
+    template: `
+        <ngts-text
+            [text]="text"
+            [color]="color"
+            [fontSize]="12"
+            [maxWidth]="200"
+            [lineHeight]="1"
+            [letterSpacing]="0.02"
+            [textAlign]="'left'"
+            [font]="'https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff'"
+            [anchorX]="'center'"
+            [anchorY]="'middle'"
+            (beforeRender)="turn($any($event).object)"
+        />
+    `,
+    imports: [NgtsText],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class DefaultTextStory {
+    @Input() text = '';
+    @Input() color = '#ec2d2d';
+    readonly turn = turn;
+}
+
+export default {
+    title: 'Abstractions/Text',
+    decorators: [moduleMetadata({ imports: [StorybookSetup] })],
+} as Meta;
+
+export const Default: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: DefaultTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+Default.args = {
+    text: `LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.`,
+    color: '#ec2d2d',
+};
+
+export const Outline: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: OutlineTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+Outline.args = {
+    text: `LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.`,
+};
+
+export const Stroke: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: StrokeTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+Stroke.args = {
+    text: `LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.`,
+};
+
+export const Shadow: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: ShadowTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+Shadow.args = {
+    text: `LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.`,
+};
+
+export const LTR: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: LTRTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+LTR.args = {
+    text: `ان عدة الشهور عند الله اثنا عشر شهرا في كتاب الله يوم خلق السماوات والارض SOME LATIN TEXT HERE منها اربعة حرم
+        ذلك الدين القيم فلاتظلموا فيهن انفسكم وقاتلوا المشركين كافة كما يقاتلونكم كافة واعلموا ان الله مع المتقين`,
+};
+
+export const CustomMaterial: Story = (args) => ({
+    props: {
+        options: makeCanvasOptions({ camera: { position: [0, 0, 200] } }),
+        story: CustomMaterialTextStory,
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+    `,
+});
+
+CustomMaterial.args = {
+    text: `LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE
+      MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO
+      CONSEQUAT. DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.
+      EXCEPTEUR SINT OCCAECAT CUPIDATAT NON PROIDENT, SUNT IN CULPA QUI OFFICIA DESERUNT MOLLIT ANIM ID EST LABORUM.`,
+    color: 'turquoise',
+};
