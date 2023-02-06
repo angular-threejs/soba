1dd8ac7b0f78ec6134e0094a7b18fe7d1d242bb1
Chau Tran <nartc7789@gmail.com>
2023-02-06 11:35:16 -0600

    docs: add enviroment story

--- libs/angular-three-soba/src/staging/environment.stories.ts
+++ libs/angular-three-soba/src/staging/environment.stories.ts
@@ -1,8 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
 import { extend, NgtArgs } from 'angular-three';
+import { NgtsPerspectiveCamera } from 'angular-three-soba/cameras';
 import { NgtsOrbitControls } from 'angular-three-soba/controls';
-import { NgtsEnvironment, ngtsEnvironmentPresetsObj } from 'angular-three-soba/staging';
+import { NgtsContactShadows, NgtsEnvironment, ngtsEnvironmentPresetsObj } from 'angular-three-soba/staging';
 import { BoxGeometry, Mesh, MeshStandardMaterial, TorusKnotGeometry } from 'three';
 import { makeCanvasOptions, StorybookSetup } from '../setup-canvas';
 
@@ -10,34 +11,33 @@ const presets = Object.keys(ngtsEnvironmentPresetsObj);
 
 extend({ Mesh, MeshStandardMaterial, TorusKnotGeometry, BoxGeometry });
 
-// @Component({
-// selector: 'storybook-ground-environment',
-// standalone: true,
-// template: `
-// <ngts-environment [ground]="{height, radius}" [preset]="preset" />
-// <ngt-mesh [position]="[0, 5, 0]">
-// <ngt-box-geometry *args="[10, 10, 10]" />
-// <ngt-mesh-standard-material metalness="1" roughness="0" />
-// </ngt-mesh>
-// <ngts-contact-shadows
-// [resolution]="1024"
-// [position]="[0, 0, 0]"
-// [scale]="100"
-// [blur]="2"
-// [opacity]="1"
-// [far]="10"
-// />
-// <ngts-orbit-controls [autoRotate]="true" />
-// <ngts-perspective-camera [position]="[40, 40, 40]" [makeDefault]="true" />
-// `,
-// imports: [NgtsEnvironment, NgtsOrbitControls, NgtsPerspectiveCamera, NgtsContactShadows, NgtArgs],
-// schemas: [CUSTOM_ELEMENTS_SCHEMA],
-// })
-// class GroundEnvironmentStory {
-// @Input() preset = 'park';
-// @Input() height = 15;
-// @Input() radius = 60;
-// }
+@Component({
+    standalone: true,
+    template: `
+        <ngts-environment [ground]="{height, radius}" [preset]="preset" />
+        <ngt-mesh [position]="[0, 5, 0]">
+            <ngt-box-geometry *args="[10, 10, 10]" />
+            <ngt-mesh-standard-material metalness="1" roughness="0" />
+        </ngt-mesh>
+        <ngts-contact-shadows
+            [resolution]="1024"
+            [position]="[0, 0, 0]"
+            [scale]="100"
+            [blur]="2"
+            [opacity]="1"
+            [far]="10"
+        />
+        <ngts-orbit-controls [autoRotate]="true" />
+        <ngts-perspective-camera [position]="[40, 40, 40]" [makeDefault]="true" />
+    `,
+    imports: [NgtsEnvironment, NgtsOrbitControls, NgtsPerspectiveCamera, NgtsContactShadows, NgtArgs],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+class GroundEnvironmentStory {
+    @Input() preset = 'park';
+    @Input() height = 15;
+    @Input() radius = 60;
+}
 
 @Component({
     standalone: true,
@@ -87,10 +87,7 @@ export default {
 export const Default: Story = (args) => ({
     props: {
         story: DefaultEnvironmentStory,
-        options: makeCanvasOptions({
-            controls: false,
-            camera: { position: [0, 0, 10] },
-        }),
+        options: makeCanvasOptions({ controls: false, camera: { position: [0, 0, 10] } }),
         inputs: args,
     },
     template: `
@@ -112,10 +109,7 @@ Default.argTypes = {
 export const Files: Story = (args) => ({
     props: {
         story: FilesEnvironmentStory,
-        options: makeCanvasOptions({
-            controls: false,
-            camera: { position: [0, 0, 10] },
-        }),
+        options: makeCanvasOptions({ controls: false, camera: { position: [0, 0, 10] } }),
         inputs: args,
     },
     template: `
@@ -127,25 +121,24 @@ Files.args = {
     background: true,
 };
 
-// export const Ground: Story = (args) => ({
-// props: {
-// controls: false,
-// camera: { position: [0, 0, 10] },
-// storyComponent: GroundEnvironmentStory,
-// storyInputs: args,
-// },
-// template: `
-// <storybook-setup [controls]="controls" [camera]="camera" [storyComponent]="storyComponent" [storyInputs]="storyInputs" />
-// `,
-// });
-// Ground.args = {
-// height: 15,
-// radius: 60,
-// preset: 'park',
-// };
+export const Ground: Story = (args) => ({
+    props: {
+        story: GroundEnvironmentStory,
+        options: makeCanvasOptions({ controls: false, camera: { position: [0, 0, 10] } }),
+        inputs: args,
+    },
+    template: `
+<storybook-setup [options]="options" [story]="story" [inputs]="inputs" />
+`,
+});
+Ground.args = {
+    height: 15,
+    radius: 60,
+    preset: 'park',
+};
 
-// Ground.argTypes = {
-// preset: { options: presets, control: { type: 'select' } },
-// height: { control: { type: 'range', min: 0, max: 50, step: 0.1 } },
-// radius: { control: { type: 'range', min: 0, max: 200, step: 1 } },
-// };
+Ground.argTypes = {
+    preset: { options: presets, control: { type: 'select' } },
+    height: { control: { type: 'range', min: 0, max: 50, step: 0.1 } },
+    radius: { control: { type: 'range', min: 0, max: 200, step: 1 } },
+};
