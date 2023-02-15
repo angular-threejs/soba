dd08598a0e983e8b7f1eadda780d4fbda7011d23
Chau Tran <nartc7789@gmail.com>
2023-02-15 16:23:10 -0600

    chore: update nx

--- libs/angular-three-soba/.storybook/main.js
+++ libs/angular-three-soba/.storybook/main.js
@@ -1,16 +1,11 @@
-const rootMain = require('../../../.storybook/main');
 const { resolve } = require('path');
 
 module.exports = {
-    ...rootMain,
-    core: { ...rootMain.core, builder: 'webpack5' },
-    stories: [...rootMain.stories, '../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: [...rootMain.addons],
+    core: { builder: 'webpack5' },
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: ['@storybook/addon-essentials'],
     webpackFinal: async (config, { configType }) => {
         // apply any global webpack configs that might have been specified in .storybook/main.js
-        if (rootMain.webpackFinal) {
-            config = await rootMain.webpackFinal(config, { configType });
-        }
 
         // add your own webpack tweaks if needed
         config.module.rules.push({
